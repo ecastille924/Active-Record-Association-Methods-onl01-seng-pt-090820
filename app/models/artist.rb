c286c7ffe4e8f78ea94ea94f049a32f18edc28ad
Erick Castille <ecastille924@gmail.com>
2020-11-22 18:54:16 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    self.song.first 
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.song.sum
   end
 
   def genre_count
